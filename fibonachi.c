041fc6b1e92b94f84eaa943fa41e4d3c95e6a604
snehadasa <45522052+snehadasa@users.noreply.github.com>
2019-02-24 13:25:48 -0800

    Create fibonachi.c

---
+++ fibonachi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+    return (fib(n - 1) + fib(n - 2));
+}
+
+int main()
+{
+    int i;
+    for (i = 1; i <= 50; i++)
+    {
+    printf("%d, ", fib(i));
+    }
+}
