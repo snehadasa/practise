c42bf3e34f9ab9572bc4a6db228a9e3c659503ff
snehadasa <45522052+snehadasa@users.noreply.github.com>
2019-02-24 12:31:24 -0800

    Create find_largest_integer.c

---
+++ find_largest_integer.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int find_largest(int arr[])
+{
+    int i, largest;
+    int n = sizeof(arr)/sizeof(int);
+    
+    if (n == 0)
+    {
+        return (-1);
+    }
+    for (i = 0; i < n; i++)
+    {
+        if ( largest < arr[i])
+        {
+            largest = arr[i];
+        }
+        
+    }
+    return (largest);
+}
+
+int main()
+{
+    int arr[] = {5, 6, 200, 56, 98, 109, 255, 20000, 108, 771};
+    int largest = find_largest(arr);
+    
+    printf("largest integer %d\n", largest);
+
+    return 0;
+}
