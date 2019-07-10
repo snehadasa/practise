e64a3d7e3b3bf577148fd21a8aa018a6ca662161
snehadasa <sneha.dasasneha@gmail.com>
2019-07-10 13:28:24 -0700

    edit

--- find_largest_integer.c
+++ find_largest_integer.c
@@ -3,7 +3,7 @@
 int find_largest(int arr[])
 {
     int i, largest;
-    
+
     if (n == 0)
     {
         return (-1);
@@ -14,7 +14,6 @@ int find_largest(int arr[])
         {
             largest = arr[i];
         }
-        
     }
     return (largest);
 }
@@ -23,7 +22,7 @@ int main()
 {
     int arr[] = {5, 6, 200, 56, 98, 109, 255, 20000, 108, 771};
     int largest = find_largest(arr, 10);
-    
+
     printf("largest integer %d\n", largest);
 
     return 0;
